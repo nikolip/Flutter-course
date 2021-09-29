bf4899b459b806885e6a9e8616fdcfddd7fe3158
Niko Lipponen <niko.lipponen@devecto.com>
2021-09-29 12:07:22 +0300

    flutter course initial commit

---
+++ lib/answer.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.red,
+          ),
+          onPressed: null,
+          child: Text('Answer 1'),
+        ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import './question.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Question(
+              questions[_questionIndex],
+            ),
+            ElevatedButton(
+              onPressed: _answerQuestion,
+              child: Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: _answerQuestion,
+              child: Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: _answerQuestion,
+              child: Text('Answer 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
